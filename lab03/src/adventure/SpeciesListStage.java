c23f4cf9515b4d4095959275d62ac2f7498c586f
PoKai <huangpokai31@gmail.com>
2023-09-07 16:51:24 -0700

    success

--- lab03/src/adventure/SpeciesListStage.java
+++ lab03/src/adventure/SpeciesListStage.java
@@ -105,6 +105,11 @@ public class SpeciesListStage implements AdventureStage {
                 copy.remove(o);
             }
         }
-        return similarObjects / listOne.size();
-    }
+        if (listOne.isEmpty()){
+            return 0.0;
+        }
+        else {
+            return similarObjects / listOne.size();
+        }
+        }
 }
